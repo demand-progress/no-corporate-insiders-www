60f68ce73049ebf7b31e7ed5337b20223208d5e2
Chris Antaki <ChrisAntaki@gmail.com>
2015-06-23 11:12:05 -0700

    Refactoring & cleaning up styles

--- js/call.js
+++ js/call.js
@@ -1,45 +1,89 @@
-var CallPages = React.createClass({displayName: "CallPages",
+var Header = React.createClass({displayName: "Header",
     render: function() {
         return (
-            React.createElement("div", {class: "wrapper"}, 
+            React.createElement("header", null, 
                 React.createElement("div", {className: "title"}, 
                     "Crack Down on Wall Street"
                 ), 
 
                 React.createElement("div", {className: "paragraph"}, 
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit."
-                ), 
+                )
+            )
+        );
+    },
+});
 
+var Form = React.createClass({displayName: "Form",
+    render: function() {
+        return (
+            React.createElement("div", {className: "form"}, 
                 React.createElement("form", {onSubmit:  this.onSubmit}, 
                     React.createElement("input", {placeholder: "Your Phone Number", id: "field-phone"}), 
                     React.createElement("button", null, 
                         "Connect", 
                         React.createElement("img", {src: "images/phone.svg"})
                     )
-                ), 
-
-                React.createElement("div", {className: "privacy"}, 
-                    "This tool uses ", React.createElement("a", {href: "https://www.twilio.com/legal/privacy", target: "_blank"}, "Twilio"), "'s APIs.", 
-                    React.createElement("br", null), 
-                    "If you prefer not to use our call tool, ", React.createElement("a", {href: "#open-phone-number-modal"}, "click here"), "."
-                ), 
-
-                React.createElement("div", {className: "logos"}, 
-                    React.createElement("img", {src: "images/logos/dp.png"})
-                ), 
-
-                React.createElement("div", {className: "contact"}, 
-                    "For press inquiries, please contact us at:", 
-                    React.createElement("br", null), 
-                    "202-681-7582 or ", React.createElement("a", {href: "mailto:press@demandprogress.org"}, "press@demandprogress.org")
                 )
             )
         );
     },
 
     onSubmit: function(e) {
-        console.log(e);
         e.preventDefault();
+        console.log(e);
+    },
+});
+
+var LogoCloud = React.createClass({displayName: "LogoCloud",
+    render: function() {
+        return (
+            React.createElement("div", {className: "logos"}, 
+                React.createElement("img", {src: "images/logos/dp.png"})
+            )
+        );
+    },
+});
+
+var Privacy = React.createClass({displayName: "Privacy",
+    render: function() {
+        return (
+            React.createElement("div", {className: "privacy"}, 
+                "This tool uses ", React.createElement("a", {href: "https://www.twilio.com/legal/privacy", target: "_blank"}, "Twilio"), "'s APIs.", 
+                React.createElement("br", null), 
+                "If you prefer not to use our call tool, ", React.createElement("a", {href: "#open-phone-number-modal"}, "click here"), "."
+            )
+        );
+    },
+});
+
+var Contact = React.createClass({displayName: "Contact",
+    render: function() {
+        return (
+            React.createElement("div", {className: "contact"}, 
+                "For press inquiries, please contact us at:", 
+                React.createElement("br", null), 
+                "202-681-7582 or ", React.createElement("a", {href: "mailto:press@demandprogress.org"}, "press@demandprogress.org")
+            )
+        );
+    },
+});
+
+var CallPages = React.createClass({displayName: "CallPages",
+    render: function() {
+        return (
+            React.createElement("div", {className: "wrapper"}, 
+                React.createElement(Header, null), 
+
+                React.createElement(Form, null), 
+
+                React.createElement(Privacy, null), 
+
+                React.createElement(LogoCloud, null), 
+
+                React.createElement(Contact, null)
+            )
+        );
     },
 });
 
--- js/call.jsx
+++ js/call.jsx
@@ -1,7 +1,7 @@
-var CallPages = React.createClass({
+var Header = React.createClass({
     render: function() {
         return (
-            <div class="wrapper">
+            <header>
                 <div className="title">
                     Crack Down on Wall Street
                 </div>
@@ -9,7 +9,15 @@ var CallPages = React.createClass({
                 <div className="paragraph">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit.
                 </div>
+            </header>
+        );
+    },
+});
 
+var Form = React.createClass({
+    render: function() {
+        return (
+            <div className="form">
                 <form onSubmit={ this.onSubmit }>
                     <input placeholder="Your Phone Number" id="field-phone" />
                     <button>
@@ -17,29 +25,65 @@ var CallPages = React.createClass({
                         <img src="images/phone.svg" />
                     </button>
                 </form>
-
-                <div className="privacy">
-                    This tool uses <a href="https://www.twilio.com/legal/privacy" target="_blank">Twilio</a>&apos;s APIs.
-                    <br />
-                    If you prefer not to use our call tool, <a href="#open-phone-number-modal">click here</a>.
-                </div>
-
-                <div className="logos">
-                    <img src="images/logos/dp.png" />
-                </div>
-
-                <div className="contact">
-                    For press inquiries, please contact us at:
-                    <br />
-                    202-681-7582 or <a href="mailto:press@demandprogress.org">press@demandprogress.org</a>
-                </div>
             </div>
         );
     },
 
     onSubmit: function(e) {
-        console.log(e);
         e.preventDefault();
+        console.log(e);
+    },
+});
+
+var LogoCloud = React.createClass({
+    render: function() {
+        return (
+            <div className="logos">
+                <img src="images/logos/dp.png" />
+            </div>
+        );
+    },
+});
+
+var Privacy = React.createClass({
+    render: function() {
+        return (
+            <div className="privacy">
+                This tool uses <a href="https://www.twilio.com/legal/privacy" target="_blank">Twilio</a>&apos;s APIs.
+                <br />
+                If you prefer not to use our call tool, <a href="#open-phone-number-modal">click here</a>.
+            </div>
+        );
+    },
+});
+
+var Contact = React.createClass({
+    render: function() {
+        return (
+            <div className="contact">
+                For press inquiries, please contact us at:
+                <br />
+                202-681-7582 or <a href="mailto:press@demandprogress.org">press@demandprogress.org</a>
+            </div>
+        );
+    },
+});
+
+var CallPages = React.createClass({
+    render: function() {
+        return (
+            <div className="wrapper">
+                <Header />
+
+                <Form />
+
+                <Privacy />
+
+                <LogoCloud />
+
+                <Contact />
+            </div>
+        );
     },
 });
 
