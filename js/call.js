df6e4e5c3fde82848eea0a6b32882b7f3dd1c0fa
Chris Antaki <ChrisAntaki@gmail.com>
2015-07-01 21:07:15 -0700

    Updated copy

--- js/call.js
+++ js/call.js
@@ -42,11 +42,17 @@ var Header = React.createClass({displayName: "Header",
         return (
             React.createElement("header", null, 
                 React.createElement("div", {className: "title"}, 
-                    "Crack Down on Wall Street"
+                    "Tell President Obama: No More Wall Street Insiders at the SEC"
                 ), 
 
                 React.createElement("div", {className: "paragraph"}, 
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit."
+                    "The first time President Obama had an opening at the SEC – which oversees Wall Street – he appointed Mary Jo White, who had spent most of her career defending Wall Street.", 
+                    React.createElement("div", {className: "spacer"}), 
+
+                    "The results have been predictably dire: “get out of jail free” waivers to banks that break the law, repeatedly delaying key rules required by the Dodd-Frank Wall Street reform bill, and deadlocking the SEC with innumerable conflicts of interest.", 
+                    React.createElement("div", {className: "spacer"}), 
+
+                    "President Obama can't make this mistake again – and there are two more openings at the SEC. Sign the petition: tell President Obama to nominate commissioners to the SEC who aren't afraid to be tough on Wall Street criminals."
                 )
             )
         );
@@ -133,33 +139,58 @@ var PhoneForm = React.createClass({displayName: "PhoneForm",
 });
 
 var OptOutForm = React.createClass({displayName: "OptOutForm",
+    numbers: {
+        'The Office of the Treasury Secretary': '202-622-1100',
+        'The Office of the White House Chief of Staff': '202-456-3737',
+        'SEC Chair Mary Jo White': '202-551-2100',
+        'SEC Commissioner Luis Aguilar': '202-551-2500',
+        'SEC Commissioner Daniel Gallagher': '202-551-2600',
+        'SEC Commissioner Kara Stein': '202-551-2800',
+        'SEC Commissioner Michael Piwowar': '202-551-2700',
+        'The Office of the SEC General Counsel': '202-551-5100',
+        'The Domestic Policy Council': '202-456-5594',
+        'The Office of Public Engagement': '202-456-1097',
+        'The Office of the Press Secretary': '202-456-3282',
+        'The White House General Counsel': '202-456-2632',
+        'The Office of Management and Budget': '202-395-4840',
+        'White House Operations': '202-456-2500',
+        'The Domestic Policy Council': '202-456-6515',
+        'The Office of Administration': '202-456-2861',
+        'The Council of Economic Advisers': '202-395-5084',
+        'The White House Comment Line': '202-456-1111',
+    },
+
+    renderNumbers: function() {
+        var numbers = [];
+
+        for (var name in this.numbers) {
+            var number = this.numbers[name];
+
+            numbers.push(
+                React.createElement("div", {className: "number"}, 
+                    React.createElement("div", {className: "name"}, 
+                         name 
+                    ), 
+
+                    React.createElement("div", {className: "phone"}, 
+                         number 
+                    )
+                )
+            );
+        }
+
+        return numbers;
+    },
+
     render: function() {
         return (
             React.createElement("div", {className: "opt-out-form"}, 
                 React.createElement("div", {className: "script"}, 
-                    "Tell them: \"Lorem ipsum\""
+                    "Tell them: “It’s outrageous that the White House is considering naming more Wall Street insiders to the SEC. We need tough ‘cops on the beat’ who will enforce the laws on the big banks, not revolving door picks who will let Wall Street off the hook. President Obama must not name a Wall Street insider like Keir Gumbs or Anne Small to the SEC.”"
                 ), 
 
                 React.createElement("div", {className: "numbers"}, 
-                    React.createElement("div", {className: "number"}, 
-                        React.createElement("div", {className: "name"}, 
-                            "John Smith"
-                        ), 
-
-                        React.createElement("div", {className: "phone"}, 
-                            "(555) 555-5555"
-                        )
-                    ), 
-
-                    React.createElement("div", {className: "number"}, 
-                        React.createElement("div", {className: "name"}, 
-                            "John Smith"
-                        ), 
-
-                        React.createElement("div", {className: "phone"}, 
-                            "(555) 555-5555"
-                        )
-                    )
+                     this.renderNumbers() 
                 )
             )
         );
